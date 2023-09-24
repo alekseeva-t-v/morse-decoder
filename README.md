09c2d07d7b050c935d33fe6225959d448448ce1a
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-24 06:56:10 +0300

    docs: update readme with additional information

--- README.md
+++ README.md
@@ -1,38 +1,29 @@
-# Morse code decoder
+# **Расшифровка азбуки Морзе**  |  **Morse code decoder**
 
-## Task
+![Morse code decoder](./public/morse-code.jpg)
 
-Your task is to write a function, that decodes Morse code and returns a string.
-Write your solution in `src/index.js`
+## Описание Задачи
 
----
-
-**Input:** String. Its length is multiple of 10.
-- Each letter from alphabet encoded with dots(`.`) and dashes(`-`). `10` stands for dot(`.`), `11` stands for dash(`-`).
-- Each encoded **letter's length is 10**.
-- If the length of the encoded letter is **less then 10**, it `left padded` with `0`.
-- `Space` in string is `**********`.
-**Output:** String (decoded)
-**Example:** `me` -> `m === -- === 0000001111`, `e === . === 0000000010` -> `00000011110000000010`
+Задача — написать функцию, которая декодирует азбуку Морзе и возвращает строку.
+Реализованное решение в `src/index.js`
 
 ---
 
-## Prepare and test
+**Ввод:** Строка. Её длина кратна 10.
+- Каждая буква алфавита закодирована точками (`.`) и тире (`-`). `10` означает точку (`.`), `11` означает тире (`-`).
+- Каждая закодированная **длина буквы равна 10**.
+- Если длина закодированной буквы **меньше 10**, она дополняется слева `0`.
+- Пробел в строке — это `**********`.
+**Вывод:** Строка (декодированная)
+**Пример:** `me` -> `m === -- === 0000001111`, `e === . === 0000000010` -> `00000011110000000010`
 
-- Install [Node.js](https://nodejs.org/en/)
-- Fork this repository: `https://github.com/romacher/morse-decoder.git`
-- Clone your newly created repo: https://github.com/<%your_github_username%>/morse-decoder/
-- Go to the folder `morse-decoder`
-- Run `npm install` in command line
-- Run `npm test` in command line
-- You will see the number of passed and failed tests
+---
 
-## Submit to [rs app](https://app.rs.school)
-1. Open [rs app](https://app.rs.school) and login
-2. Open `RS APP` and click `Auto Test`
-3. Select your task (morse-decoder)
-4. Press the submit button and enjoy
-
-### Notes
-1. We recommend you to use nodejs of version 14 or lower. If you using any of features that does not supported by node v10, score won't be submitted.
-2. Please be sure that each of your test in limit of 30sec.
+## Тестирование решения
+1. Установите [Node.js](https://nodejs.org/en/download/)
+2. Сделайте форк этого репозитория
+3. Склонируйте себе этот репозиторий: https://github.com/<%your_github_username%>/morse-decoder/
+4. Перейдите в папку `morse-decoder`
+5. Вбейте в командную строку `npm install` для установки зависимостей
+6. Выполните `npm test` в командной строке.
+7. Вы увидите число ожидающих (pending), проходящих и падающих тестов. 100% проходящие тесты соответствуют максимальному баллу за задание.
--- src/index.js
+++ src/index.js
@@ -1,215 +1,95 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
+/**
+ * Возвращает декодированную строку.
+ *
+ * @param {string} expr Исходная строка, которую необходимо декодировать.
+ * @return {string} Декодированная строка.
+ */
 function decode(expr) {
-    // Создаем массив в который будет выведен результат состоящий из группы подмассивов по 10 цифр
-  let substr = [];
-  // Создаем массив в который будет выведен результат состоящий из группы подмассивов
-  let array = [];
-  // Создаем результирующий массив, состаящий из элементов объединенных подмассивов
-  let res = [];
-  // Преобразуем полученную строку в массив
-  let data = expr.split('');
-  // Разделяем изначальный массив на группу подмассивов
-  for (let i = 0; i < data.length / 10; i++) {
-    substr[i] = data.slice(i * 10, i * 10 + 10).join('');
-    array.push(String(+substr[i]));
+  let resArray = [];
+
+  /**
+   * Возвращает массив элементов, на которые будет разделена строка.
+   *
+   * @param {string} string Исходная строка, которую необходимо разделить.
+   * @param {number} elemCoun Количество символов.
+   * @return {array} массив элементов, на которые будет разделена строка.
+   */
+  function lineDivision(string, elemCount) {
+    let data = string.split('');
+    let substrArr = [];
+    let resArr = [];
+
+    for (let i = 0; i < data.length / elemCount; i++) {
+      substrArr[i] = data
+        .slice(i * elemCount, i * elemCount + elemCount)
+        .join('');
+      resArr.push(String(+substrArr[i]));
+    }
+
+    return resArr;
   }
 
-  for (let elem of array) {
-    if (elem === '1011') {
-      elem = MORSE_TABLE['.-'];
-      res.push(elem);
-    }
-    if (elem === '11101010') {
-      elem = MORSE_TABLE['-...'];
-      res.push(elem);
-    }
-    if (elem === '11101110') {
-      elem = MORSE_TABLE['-.-.'];
-      res.push(elem);
-    }
-    if (elem === '111010') {
-      elem = MORSE_TABLE['-..'];
-      res.push(elem);
-    }
-    if (elem === '10') {
-      elem = MORSE_TABLE['.'];
-      res.push(elem);
-    }
-    if (elem === '10101110') {
-      elem = MORSE_TABLE['..-.'];
-      res.push(elem);
-    }
-    if (elem === '111110') {
-      elem = MORSE_TABLE['--.'];
-      res.push(elem);
-    }
-    if (elem === '10101010') {
-      elem = MORSE_TABLE['....'];
-      res.push(elem);
-    }
-    if (elem === '1010') {
-      elem = MORSE_TABLE['..'];
-      res.push(elem);
-    }
-    if (elem === '10111111') {
-      elem = MORSE_TABLE['.---'];
-      res.push(elem);
-    }
-    if (elem === '111011') {
-      elem = MORSE_TABLE['-.-'];
-      res.push(elem);
-    }
-    if (elem === '10111010') {
-      elem = MORSE_TABLE['.-..'];
-      res.push(elem);
-    }
-    if (elem === '1111') {
-      elem = MORSE_TABLE['--'];
-      res.push(elem);
-    }
-    if (elem === '1110') {
-      elem = MORSE_TABLE['-.'];
-      res.push(elem);
-    }
-    if (elem === '111111') {
-      elem = MORSE_TABLE['---'];
-      res.push(elem);
-    }
-    if (elem === '10111110') {
-      elem = MORSE_TABLE['.--.'];
-      res.push(elem);
-    }
-    if (elem === '11111011') {
-      elem = MORSE_TABLE['--.-'];
-      res.push(elem);
-    }
-    if (elem === '101110') {
-      elem = MORSE_TABLE['.-.'];
-      res.push(elem);
-    }
-    if (elem === '101010') {
-      elem = MORSE_TABLE['...'];
-      res.push(elem);
-    }
-    if (elem === '11') {
-      elem = MORSE_TABLE['-'];
-      res.push(elem);
-    }
-    if (elem === '101011') {
-      elem = MORSE_TABLE['..-'];
-      res.push(elem);
-    }
-    if (elem === '10101011') {
-      elem = MORSE_TABLE['...-'];
-      res.push(elem);
-    }
-    if (elem === '101111') {
-      elem = MORSE_TABLE['.--'];
-      res.push(elem);
-    }
-    if (elem === '11101011') {
-      elem = MORSE_TABLE['-..-'];
-      res.push(elem);
-    }
-    if (elem === '11101111') {
-      elem = MORSE_TABLE['-.--'];
-      res.push(elem);
-    }
-    if (elem === '11111010') {
-      elem = MORSE_TABLE['--..'];
-      res.push(elem);
-    }
-    if (elem === '1011111111') {
-      elem = MORSE_TABLE['.----'];
-      res.push(elem);
-    }
-    if (elem === '1010111111') {
-      elem = MORSE_TABLE['..---'];
-      res.push(elem);
-    }
-    if (elem === '1010101111') {
-      elem = MORSE_TABLE['...--'];
-      res.push(elem);
-    }
-    if (elem === '1010101111') {
-      elem = MORSE_TABLE['...--'];
-      res.push(elem);
-    }
-    if (elem === '1010101011') {
-      elem = MORSE_TABLE['....-'];
-      res.push(elem);
-    }
-    if (elem === '1010101010') {
-      elem = MORSE_TABLE['.....'];
-      res.push(elem);
-    }
-    if (elem === '1110101010') {
-      elem = MORSE_TABLE['-....'];
-      res.push(elem);
-    }
-    if (elem === '1111101010') {
-      elem = MORSE_TABLE['--...'];
-      res.push(elem);
-    }
-    if (elem === '1111111010') {
-      elem = MORSE_TABLE['---..'];
-      res.push(elem);
-    }
-    if (elem === '1111111110') {
-      elem = MORSE_TABLE['----.'];
-      res.push(elem);
-    }
-    if (elem === '1111111111') {
-      elem = MORSE_TABLE['-----'];
-      res.push(elem);
-    }
-    if (elem === 'NaN') {
-      elem = ' ';
-      res.push(elem);
-    }
-  }
+  const codedLettersArray = lineDivision(expr, 10);
 
-  return res.join('');// write your solution here
+  console.log(codedLettersArray);
+
+  codedLettersArray.forEach((value) => {
+    if (value === 'NaN') {
+      resArray.push(' ');
+    } else {
+      const codedCharactersArray = lineDivision(value, 2);
+      const encodedCharactersString = codedCharactersArray
+        .map((value) => {
+          return value === '10' ? '.' : '-';
+        })
+        .join('');
+
+      resArray.push(MORSE_TABLE[encodedCharactersString]);
+    }
+  });
+
+  return resArray.join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
